0890dc5678b7a853a86864b465eec38b0cab93df
Harish Jadhav <harishjadhav675@gmail.com>
2024-01-20 00:23:25 +0530

    connected to the database

--- index.js
+++ index.js
@@ -1,16 +1,13 @@
 const express = require('express');
 const port = 8000;
 const app = express();
-
+const db = require('./config/mongoose');
 
 
 app.listen(port , function(err){
     
-     if(err){
-         
+     if(err){ 
         console.log("Error in running the server");
      }
-     
      console.log("Server is Running on pot" , port);
-
 })
